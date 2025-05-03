3980ce7fb646714e9509bce212ac3eb8153f9b44
matix2267 <matix2267@gmail.com>
2025-05-03 20:54:41 +0200

    Replace with level based on linear distance

--- control.lua
+++ control.lua
@@ -1,5 +1,21 @@
-local function replace_spawner(spawner)
-    local name = spawner.name .. "-2"
+local max_level = 10
+local distance_per_level = 240
+local distance_first_level_offset = 240
+
+local function calculate_level(position)
+    local distance = math.sqrt(position.x * position.x + position.y * position.y)
+    local level = (distance - distance_first_level_offset) / distance_per_level + 1
+    return math.floor(math.min(math.max(level, 1), max_level))
+end
+
+local function safe_replace_spawner(spawner)
+    if prototypes.entity[spawner.name .. "-1"] == nil then
+        return
+    end
+
+    local level = calculate_level(spawner.position)
+
+    local name = spawner.name .. "-" .. level
     local surface = spawner.surface
     local position = spawner.position
     local force = spawner.force
@@ -14,24 +30,24 @@ end
 -- New game or mod installed
 script.on_init(function()
     for _, surface in pairs(game.surfaces) do
-        local spawners = surface.find_entities_filtered { force = "enemy", type = "unit-spawner", name = "biter-spawner" }
+        local spawners = surface.find_entities_filtered { force = "enemy", type = "unit-spawner" }
         for _, spawner in pairs(spawners) do
-            replace_spawner(spawner)
+            safe_replace_spawner(spawner)
         end
     end
 end)
 
 -- New chunk generated
 script.on_event(defines.events.on_chunk_generated, function(event)
-    local spawners = event.surface.find_entities_filtered { area = event.area, force = "enemy", type = "unit-spawner", name = "biter-spawner" }
+    local spawners = event.surface.find_entities_filtered { area = event.area, force = "enemy", type = "unit-spawner" }
     for _, spawner in pairs(spawners) do
-        replace_spawner(spawner)
+        safe_replace_spawner(spawner)
     end
 end)
 
--- Enemy expansion
+-- A script spawned a new nest, doesn't seem to work for enemy expansions.
 script.on_event(defines.events.on_entity_spawned, function(event)
-    if event.entity.name == "biter-spawner" then
-        replace_spawner(event.entity)
+    if event.entity.type == "unit-spawner" then
+        safe_replace_spawner(event.entity)
     end
 end)
